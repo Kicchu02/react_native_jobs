a095ae707d07ab659c31dec96e5be2d36825c673
Kicchu02 <kicchu.2002@gmail.com>
2023-03-24 21:11:43 +0530

    first commit

---
+++ app/_layout.js
@@ -0,0 +1,26 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    // DMBold: require("../assets/fonts/DMSans-Bold.tff"),
+    // DMMedium: require("../assets/fonts/DMSans-Medium.tff"),
+    // DMRegular: require("../assets/fonts/DMSans-Regular.tff"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
